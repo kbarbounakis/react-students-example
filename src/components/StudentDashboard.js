e7347d06b9109b495bb921bb4cfb9420d6ee084c
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-11-01 06:32:38 +0000

    add student progress

--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -1,22 +1,53 @@
 import React from "react";
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { useTranslation } from "react-i18next";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, ProgressBar } from "react-bootstrap";
 import { ApplicationContext } from '../ApplicationContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap} from '@fortawesome/free-solid-svg-icons'
+import { StudentProgress } from './StudentProgress';
 
 const StudentDashboard = () => {
   const { t } = useTranslation();
+  const [student, setStudent] = useState(null);
+  const [studentProgress, setStudentProgress] = useState(0);
   /**
    * @type {{context:import('@themost/react').ReactDataContext}}
    */
   const {context} = useContext(ApplicationContext);
-  context.model('Students/Me').asQueryable().getItem().then((student) => {
-    console.log(student);
-  });
+  useEffect(() => {
+    context.model('Students/Me').asQueryable().expand((x) => {x.studyProgram}).getItem().then((result) => {
+      setStudent(result)
+      setStudentProgress(100*(result.semester/result.studyProgram.semesters))
+    });
+  }, []);
+
+  
+  
   return (
     <>
       <Row>
-      
+        <Col lg={12}>
+          <div className='m-3'>
+            <div className='d-flex justify-content-start'>
+              <div className='avatar avatar-6 avatar-male-student'></div>
+              <div className='ml-3 align-self-center'>
+              <h1>
+                {t("Dashboard")}
+                <h6 className='font-weight-normal'>{student?.person?.familyName} {student?.person?.givenName}</h6>
+              </h1>
+            </div>
+            </div>
+          </div>
+        </Col>
+        <Col lg={12}>
+          <div className='m-3'>
+            <StudentProgress student={student} />
+          </div>
+        </Col>
+        <Col lg={12}>
+
+        </Col>
       </Row>
       <Row>
         <Col lg={6}>
@@ -26,7 +57,7 @@ const StudentDashboard = () => {
 
         </Col>
       </Row>
-      
+
     </>
   );
 };
---
+++ src/components/StudentProgress.js
@@ -0,0 +1,35 @@
+import { ProgressBar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap} from '@fortawesome/free-solid-svg-icons'
+import { useTranslation } from 'react-i18next';
+
+const StudentProgress = ({ student }) => {
+    let studentProgress = 0
+    let semester = "-";
+    if (student) {
+        studentProgress = parseInt(100*(student?.semester/student?.studyProgram?.semesters), 10);
+        semester = student?.semester;
+    }
+    const { t } = useTranslation();
+    return (
+        <>
+            <div class="d-flex justify-content-between justify-content-center align-items-center">
+              <div className='mx-4 w-100'>
+                <ProgressBar className='progress-semester' striped={false} animated={false} variant='theme' now={studentProgress} />
+              </div>
+              <div>
+                <FontAwesomeIcon color="gainsboro" icon={faGraduationCap} size="4x" />
+              </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <div>
+                    <h4 class="font-weight-normal">{semester}/{t("Semester")}</h4>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export {
+    StudentProgress
+}
